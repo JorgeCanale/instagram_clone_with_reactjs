ffcdc84c3c298bede8b432255710b118c3b61f22
jorge canale <jorgecanale636@yahoo.comm>
2023-06-29 12:20:05 -0300

    i think this is not the way i should that i should do it

--- src/components/right_sidebar/profile_slice.js
+++ src/components/right_sidebar/profile_slice.js
@@ -20,12 +20,12 @@ function ProfileSlice ({uname,aname, alink}){
             <div className="a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12">
                 <div className="a1 a2 a3 a4 a5 a7 b6 b11 b13">
                     <div className="a1 a4 a5 a7 b2 b8 b9 b11 b14 b15 c9 "> 
-                        <span className="user_name">
-                            <span>
-                                <div>
-                                    <div>
-                                        <div>
-                                            <a href={alink}>{uname}</a>
+                        <span className="a2 a7 a16  a18 a19 b4 b13 " dir="auto">
+                            <span className="a17 a20 a19 b4 b13 b18">
+                                <div className="a1 a4 a11 b2 b8 b15 b18 d9">
+                                    <div className="c4">
+                                        <div className="c4">
+                                            <a className="c4" href={alink}>{uname}</a>
                                         </div>
                                     </div>
                                 </div>
